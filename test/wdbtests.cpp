18a6a383904a2973450c15cc7084583aa3dfb558
Wagner Marini <4212421+wmarini@users.noreply.github.com>
2026-02-22 06:43:07 -0800

    Adding process lauch test cases

--- test/wdbtests.cpp
+++ test/wdbtests.cpp
@@ -1,6 +1,24 @@
 #include <catch2/catch_test_macros.hpp>
+#include <libwdb/process.h>
+#include <libwdb/error.h>
+#include <signal.h>
 
-TEST_CASE("validate environment") {
-    REQUIRE(false);
+using namespace wdb;
+
+namespace {
+
+bool process_exists(pid_t pid) {
+    auto ret = kill(pid, 0);
+    return ret != -1 and errno != ESRCH;
 }
 
+}
+
+TEST_CASE("process::lauch success", "[process]") {
+    auto proc = process::launch("yes");
+    REQUIRE(process_exists(proc->pid()));
+}
+
+TEST_CASE("process::lauch no such program", "[process]") {
+    REQUIRE_THROWS_AS(process::launch("you_do_not_have_to_be_good"), error);
+}
