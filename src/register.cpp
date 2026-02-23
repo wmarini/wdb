c44e577fcb40cf474f4abaa5080b8492a64a5f00
Wagner Marini <4212421+wmarini@users.noreply.github.com>
2026-02-23 05:10:06 -0800

    Creating register.cpp

---
+++ src/register.cpp
@@ -0,0 +1,32 @@
+#include "libwdb/registers.h"
+#include "libwdb/bit.h"
+
+namespace wdb {
+
+registers::value registers::read(const register_info& info) const {
+    auto bytes = as_bytes(data_);
+
+    if (info.format == register_format::uint) {
+        switch (info.size) {
+        case 1: return from_bytes<std::uint8_t>(bytes + info.offset);
+        case 2: return from_bytes<std::uint16_t>(bytes + info.offset);
+        case 4: return from_bytes<std::uint32_t>(bytes + info.offset);
+        case 8: return from_bytes<std::uint64_t>(bytes + info.offset);
+        default: error::send("Unexpected register size");
+        }
+    }
+    else if (info.format == register_format::double_float) {
+        return from_bytes<double>(bytes + info.offset);
+    }
+    else if (info.format == register_format::long_double) {
+        return from_bytes<long double>(bytes + info.offset);
+    }
+    else if (info.format == register_format::vector and info.size == 8) {
+        return from_bytes<byte64>(bytes + info.offset);
+    }
+    else {
+        return from_bytes<byte128>(bytes + info.offset);
+    }
+}
+
+} // namespace wdb
